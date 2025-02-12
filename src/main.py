9f8f2844a74c897ad5adcaba9853443f734ebaff
Generous programmer <ch05341315656@GMAIL.COM>
2025-02-13 00:35:19 +0200

    Delete src directory

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from file_converter_app import FileConverterApp
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = FileConverterApp()
-    window.show()
-    sys.exit(app.exec_())
--- src/ui/__init__.py
+++
@@ -1,4 +0,0 @@
-from .main_window import MainWindow
-from .dialogs import SettingsDialog, AboutDialog
-
-__all__ = ['MainWindow', 'SettingsDialog', 'AboutDialog']
